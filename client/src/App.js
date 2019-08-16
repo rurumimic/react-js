7cdf1cc79b388d9cf33a66ffd57479dac49dbf27
rurumimic <unhyop@gmail.com>
2019-08-16 20:52:40 +0900

    ESLint React

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  const [message, setMessage] = useState('loading...');
+function App () {
+  const [message, setMessage] = useState('loading...')
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('/api/hello');
-      const json = await response.json();
-      setMessage(json.greet);
+    async function fetchData () {
+      const response = await fetch('/api/hello')
+      const json = await response.json()
+      setMessage(json.greet)
     }
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <div className="App">
       {message}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
