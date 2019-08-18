fbb3dcdcda363a2e64fb945de00a14ebbd333081
rurumimic <unhyop@gmail.com>
2019-08-18 14:28:18 +0900

    Typescript Version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const App: React.FC = () => {
-
-  const [message, setMessage] = useState('loading...'); // 초기값 설정
+  const [message, setMessage] = useState('loading...') // 초기값 설정
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('/api/hello'); // localhost:8081/api/hello
-      const json = await response.json(); // { "greet": "Hello, there." }
-      setMessage(json.greet); // "Hello, there."
+    async function fetchData(): Promise<void> {
+      const response = await fetch('/api/hello') // localhost:8081/api/hello
+      const json = await response.json() // { "greet": "Hello, there." }
+      setMessage(json.greet) // "Hello, there."
     }
     fetchData()
-  }, []);
+  }, [])
 
-  return <div className="App">{message}</div>;
+  return <div className="App">{message}</div>
 }
 
-export default App;
+export default App
